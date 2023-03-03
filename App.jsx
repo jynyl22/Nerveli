5bf99a463633544754f15aa20d560bfa9ef760d5
Acharos <williamzhuo@rocketmail.com>
2023-03-03 17:41:57 -0600

    basic react navigation

--- App.jsx
+++ App.jsx
@@ -5,31 +5,23 @@
  * @format
  */
 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  StyleSheet
 } from 'react-native';
+import NPA1 from './screens/NPA1';
+import NPA2 from './screens/NPA2';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
-function Section({children, title}){
-}
-
-function App(){
-}
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return(
+  <Stack.Navigator initialRouteName="NPA1">
+    <Stack.Screen name = "NPA1" component={NPA1}/>
+    <Stack.Screen name = "NPA2" component={NPA2}/>
+  </Stack.Navigator>
+  );
+};
 
 const styles = StyleSheet.create({
 
---
+++ screens/NPA2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View,Text, TouchableOpacity } from 'react-native';
+
+const NPA2 = ({navigation}) =>{
+    return(
+        <View>
+            <Text>Home 2 Screen</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('NPA1')}>
+                <Text>Go to different page</Text>
+                </TouchableOpacity>
+        </View>
+    );
+};
+
+export default NPA2
