7aa94d0e5d79301b0030f11817bc508be78aa8de
Christy <christyjlee07@gmail.com>
2023-03-03 14:49:07 -0600

    blank screen

---
+++ App.jsx
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+function Section({children, title}){
+}
+
+function App(){
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
