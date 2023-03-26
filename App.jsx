d17e46d025c67604063438205a75f80ff32d581d
Christy <christyjlee07@gmail.com>
2023-03-26 03:44:55 -0500

    home page file

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import HomePage from './screens/home-page/HomePage';
 import Overview from './screens/NPA-test/Overview';
 import NPA1_1 from './screens/NPA-test/NPA1_1';
 import NPA1_2 from './screens/NPA-test/NPA1_2';
@@ -33,12 +34,13 @@ import NPA2_17 from './screens/NPA-test/NPA2_17';
 const Stack = createNativeStackNavigator();
 const App = () => {
   return(
-  <Stack.Navigator initialRouteName="Overview" screenOptions={{title:"NERVELI",
+  <Stack.Navigator initialRouteName="HomePage" screenOptions={{title:"NERVELI",
   headerTitleAlign: 'center',
   headerShadowVisible: false,
   headerTitleStyle: {fontFamily: "Lato-Bold",fontSize: 18, textAlign: 'center', color: 'black'},
   contentStyle: { backgroundColor: '#FFFFF' },
   }}>
+    <Stack.Screen name = "HomePage" component={HomePage}/>
     <Stack.Screen name = "Overview" component={Overview}/>
     <Stack.Screen name = "NPA1_1" component={NPA1_1}/>
     <Stack.Screen name = "NPA1_2" component={NPA1_2}/>
---
+++ screens/home-page/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image, View, Text, Pressable, StyleSheet,ScrollView} from 'react-native';
+
+
+const HomePage = ({navigation}) =>{
+    return(
+        <ScrollView>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default HomePage
