c1d177b97a6e74a4efb268c2211a03252aef1e37
rahuld0219 <rahuld0219@gmail.com>
2023-04-30 18:05:26 -0500

    fixed recommendations page header

--- components/nav/HomeStack.js
+++ components/nav/HomeStack.js
@@ -5,8 +5,12 @@ import RecPage from '../../screens/home-page/RecPage';
 
 const Home = createNativeStackNavigator();
 export default HomeStack = () => {
-    return(<Home.Navigator initialRouteName="Home">
-        <Home.Screen name="Home" options={{headerShown:false}} component={HomePage}/>
+    return(<Home.Navigator initialRouteName="HomePage" screenOptions={{title:"NERVELI",
+            headerTitleAlign: 'center',
+            headerShadowVisible: false,
+            headerTitleStyle: {fontFamily: "Lato-Bold",fontSize: 18, textAlign: 'center', color: 'black'},
+            contentStyle: { backgroundColor: '#FFFFF' },}}>
+        <Home.Screen name="HomePage" options={{headerShown:false}} component={HomePage}/>
         <Home.Screen name="Recommendations" component={RecPage}/>
     </Home.Navigator>);
 }
