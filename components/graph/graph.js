c225cafc87a33b476aaf6cea37241866ac7712ec
rahuld0219 <rahuld0219@gmail.com>
2023-04-30 15:52:17 -0500

    added graph

---
+++ components/graph/graph.js
@@ -0,0 +1,65 @@
+import React, { useRef } from 'react';
+import { useEffect, useState } from "react";
+import { View, StyleSheet, PanResponder } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
+
+const DraggableBox = ({ boxSize }) => {
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gestureState) => {
+        const { dx, dy } = gestureState;
+        const newX = Math.min(Math.max(0, dx), boxSize.width-(boxSize.width/6));
+        const newY = Math.min(Math.max(0, dy), boxSize.height-(boxSize.width/6));
+        setTranslateX(newX);
+        setTranslateY(newY);
+      },
+    })
+  ).current;
+
+  const [translateX, setTranslateX] = useState(0);
+  const [translateY, setTranslateY] = useState(0);
+
+  const path = `M ${0},${0} Q ${0},${translateY+boxSize.width/12} ${translateX+boxSize.width/12},${translateY+boxSize.width/12}`;
+  const path2 = `M ${translateX+boxSize.width/12},${translateY+boxSize.width/12} Q ${boxSize.width},${translateY+boxSize.width/12} ${boxSize.width},${boxSize.height}`;
+
+  const styles = StyleSheet.create({
+    circle: {
+      backgroundColor: 'white',
+      borderRadius: boxSize.width / 2, // Circle shape
+      width: boxSize.width/6,
+      height: boxSize.width/6,
+      transform: [{ translateX }, { translateY }],
+      position: 'absolute',
+      top:boxSize.y,
+      left:boxSize.x
+
+    },
+    box: {
+          backgroundColor: 'grey',
+          borderRadius: 10,
+          width: boxSize.width,
+          height: boxSize.height,
+          top:boxSize.y,
+          left:boxSize.x
+        },
+  });
+
+  return (
+    <View>
+        <View style={styles.box} />
+
+        <View style={{top:(boxSize.width/6)-boxSize.height, left:boxSize.width/6}}>
+            <Svg height="100%" width="100%" >
+                <Path d={path} stroke="black" strokeWidth="3" fill="transparent" />
+                <Path d={path2} stroke="black" strokeWidth="3" fill="transparent" />
+            </Svg>
+        </View>
+
+        <View style={styles.circle} {...panResponder.panHandlers} />
+
+    </View>
+  );
+};
+
+export default DraggableBox;
